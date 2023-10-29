76d0a4c2c63db3d2f17059bde62488227e054e6e
Aschen <amaret93@gmail.com>
2023-10-29 19:30:47 +0100

    remove log

--- lib/LLMAgent.ts
+++ lib/LLMAgent.ts
@@ -1,18 +1,18 @@
-import * as Path from "node:path";
+import * as Path from 'node:path';
 
-import { OpenAI } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain/prompts";
+import { OpenAI } from 'langchain/llms/openai';
+import { PromptTemplate } from 'langchain/prompts';
 
-import { CacheEngine } from "./cache/CacheEngine";
-import { LLMAction, ActionFeedback } from "./actions/LLMAction";
-import { DoneAction } from "./actions/DoneAction";
+import { CacheEngine } from './cache/CacheEngine';
+import { LLMAction, ActionFeedback } from './actions/LLMAction';
+import { DoneAction } from './actions/DoneAction';
 
-const LOCAL_DEBUG = process.env.NODE_ENV !== "production";
+const LOCAL_DEBUG = process.env.NODE_ENV !== 'production';
 
 function kebabCase(str) {
   return str
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/[\s_]+/g, "-")
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[\s_]+/g, '-')
     .toLowerCase();
 }
 
@@ -24,11 +24,11 @@ export type AgentOptions = {
 };
 
 const MODELS_COST = {
-  "gpt-4": {
+  'gpt-4': {
     input: 0.03,
     output: 0.06,
   },
-  "gpt-3.5-turbo-16k": {
+  'gpt-3.5-turbo-16k': {
     input: 0.003,
     output: 0.004,
   },
@@ -113,7 +113,7 @@ export abstract class LLMAgent {
       });
 
       const answer = await this.callModel({
-        model: "gpt-4",
+        model: 'gpt-4',
         prompt,
       });
 
@@ -132,14 +132,14 @@ export abstract class LLMAgent {
           })
         );
 
-        if (feedback.type === "error") {
+        if (feedback.type === 'error') {
           error = true;
           this.actionsErrorCount++;
         } else {
           this.actionsCount++;
         }
 
-        if (action.name === "done") {
+        if (action.name === 'done') {
           done = true;
         }
       }
@@ -168,13 +168,11 @@ export abstract class LLMAgent {
     if (this.cacheEngine && cache) {
       this.initCache();
 
-      const promptCacheKey = this.cacheKey({ type: "prompt", prompt });
-
-      this.log(`Caching prompt at "${promptCacheKey}"`);
+      const promptCacheKey = this.cacheKey({ type: 'prompt', prompt });
 
       await this.cacheEngine.set(promptCacheKey, prompt);
 
-      const answerCacheKey = this.cacheKey({ type: "answer", prompt });
+      const answerCacheKey = this.cacheKey({ type: 'answer', prompt });
       if (await this.cacheEngine.has(answerCacheKey)) {
         this.log(`Using cached answer at "${answerCacheKey}"`);
 
@@ -190,9 +188,7 @@ export abstract class LLMAgent {
     await this.computeCosts({ model, prompt, answer });
 
     if (this.cacheEngine && cache) {
-      const answerCacheKey = this.cacheKey({ type: "answer", prompt });
-
-      this.log(`Caching answer at "${answerCacheKey}"`);
+      const answerCacheKey = this.cacheKey({ type: 'answer', prompt });
 
       await this.cacheEngine.set(answerCacheKey, answer);
     }
@@ -205,7 +201,7 @@ export abstract class LLMAgent {
     prompt,
     answer,
   }: {
-    model: "gpt-4" | "gpt-3.5-turbo-16k";
+    model: 'gpt-4' | 'gpt-3.5-turbo-16k';
     prompt: string;
     answer: string;
   }) {
@@ -232,12 +228,12 @@ export abstract class LLMAgent {
     type,
     prompt,
   }: {
-    type: "prompt" | "answer";
+    type: 'prompt' | 'answer';
     prompt: string;
   }) {
     const promptHash = this.cacheEngine.hash(prompt);
 
-    let filename = "";
+    let filename = '';
 
     if (this.localDebug) {
       filename += `${this.promptStep}-`;
@@ -277,9 +273,9 @@ export abstract class LLMAgent {
     for (const line of lines) {
       const trimmedLine = line.trim();
 
-      if (trimmedLine.startsWith("<Action")) {
+      if (trimmedLine.startsWith('<Action')) {
         insideActionBlock = true;
-        currentAction = { name: "", parameters: {} };
+        currentAction = { name: '', parameters: {} };
 
         const nameMatch = trimmedLine.match(/name="([^"]+)"/);
         if (nameMatch) {
@@ -292,31 +288,31 @@ export abstract class LLMAgent {
         if (inlineParameters) {
           for (const inlineParameter of inlineParameters) {
             const [key, value] = inlineParameter
-              .replace("parameter:", "")
-              .split("=");
-            currentAction.parameters[key.replace(/"/g, "")] = value.replace(
+              .replace('parameter:', '')
+              .split('=');
+            currentAction.parameters[key.replace(/"/g, '')] = value.replace(
               /"/g,
-              ""
+              ''
             );
           }
         }
 
-        if (trimmedLine.endsWith("/>")) {
+        if (trimmedLine.endsWith('/>')) {
           insideActionBlock = false;
           if (currentAction.name) {
             actions.push(currentAction);
           }
           currentAction = null;
         }
-      } else if (trimmedLine.startsWith("</Action>")) {
+      } else if (trimmedLine.startsWith('</Action>')) {
         insideActionBlock = false;
         if (currentAction && currentAction.name) {
           actions.push(currentAction);
         }
         currentAction = null;
-      } else if (insideActionBlock && trimmedLine.startsWith("<Parameter")) {
+      } else if (insideActionBlock && trimmedLine.startsWith('<Parameter')) {
         insideParameterBlock = true;
-        currentParameterValue = "";
+        currentParameterValue = '';
         const nameMatch = trimmedLine.match(/name="([^"]+)"/);
         if (nameMatch) {
           currentParameterName = nameMatch[1];
@@ -324,7 +320,7 @@ export abstract class LLMAgent {
       } else if (
         insideActionBlock &&
         insideParameterBlock &&
-        trimmedLine.startsWith("</Parameter>")
+        trimmedLine.startsWith('</Parameter>')
       ) {
         insideParameterBlock = false;
         if (
@@ -339,13 +335,13 @@ export abstract class LLMAgent {
         currentParameterValue = null;
       } else if (insideParameterBlock) {
         if (currentParameterValue !== null) {
-          currentParameterValue += currentParameterValue ? "\n" + line : line;
+          currentParameterValue += currentParameterValue ? '\n' + line : line;
         }
       }
     }
 
     if (actions.length === 0) {
-      throw new Error("Incorrect answer format. Cannot parse actions.");
+      throw new Error('Incorrect answer format. Cannot parse actions.');
     }
 
     return actions;
@@ -363,7 +359,7 @@ export abstract class LLMAgent {
     if (!action) {
       return {
         message: `Action "${name}" not found`,
-        type: "error",
+        type: 'error',
       };
     }
 
@@ -389,7 +385,7 @@ export abstract class LLMAgent {
   }
 
   protected describeActions(): string {
-    return this.actions.map((action) => action.describe).join("\n");
+    return this.actions.map((action) => action.describe).join('\n');
   }
 
   protected describeFeedbackSteps({
@@ -401,7 +397,7 @@ export abstract class LLMAgent {
 
     for (let i = 0; i < feedbackSteps.length; i++) {
       describedSteps.push(`<Step number="${i + 1}">
-  ${feedbackSteps[i].join("\n  ")}
+  ${feedbackSteps[i].join('\n  ')}
 </Step>`);
     }
 
